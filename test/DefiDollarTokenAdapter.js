d123ca161097086b192ab0c33cba87366f520f91
Manthankumar Satani <satanimanthan@gmail.com>
2021-04-24 14:29:23 +0530

    fix: linting in defidollar test

--- test/DefiDollarTokenAdapter.js
+++ test/DefiDollarTokenAdapter.js
@@ -1,11 +1,11 @@
-const TokenAdapter = artifacts.require("DefiDollarTokenAdapter");
+const TokenAdapter = artifacts.require('DefiDollarTokenAdapter');
 
-contract("DefiDollarTokenAdapter", () => {
-  const tokenAddress = "0x5bc25f649fc4e26069ddf4cf4010f9f706c23831";
+contract('DefiDollarTokenAdapter', () => {
+  const tokenAddress = '0x5bc25f649fc4e26069ddf4cf4010f9f706c23831';
 
   let accounts;
   let tokenAdapter;
-  const DUSD = [tokenAddress, "DefiDollar", "DUSD", "18"];
+  const DUSD = [tokenAddress, 'DefiDollar', 'DUSD', '18'];
 
   beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
@@ -15,16 +15,16 @@ contract("DefiDollarTokenAdapter", () => {
     });
   });
 
-  it("should return correct components", async () => {
-    await tokenAdapter.methods["getComponents(address)"](tokenAddress)
+  it('should return correct components', async () => {
+    await tokenAdapter.methods['getComponents(address)'](tokenAddress)
       .call()
       .then((result) => {
         console.dir(result, { depth: null });
       });
   });
 
-  it("should return correct metadata", async () => {
-    await tokenAdapter.methods["getMetadata(address)"](tokenAddress)
+  it('should return correct metadata', async () => {
+    await tokenAdapter.methods['getMetadata(address)'](tokenAddress)
       .call()
       .then((result) => {
         assert.deepEqual(result, DUSD);
